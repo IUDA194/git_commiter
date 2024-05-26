b75eb631f2c6a3bc1318ca7faa1c5a12bc3f829f
Iudà <86917292+IUDA194@users.noreply.github.com>
2024-05-26 17:30:03 +0200

    + Add DEFAULT_PROMPT + Add EMOJI_PROMPT_PRESET

--- prompts_presets.py
+++ prompts_presets.py
@@ -1,1 +1,3 @@
 DEFAULT_PROMPT = "Create a brief commit description of up to 12 words for these changes. Write without markdown and in one line Exemple = '+ Delete users + Recovery users + email field in user serialyzer' Write as many + as many semantic changes : "
+
+EMOJI_PROMPT_PRESET = "Some preset"
